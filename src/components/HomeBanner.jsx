1e85cb8775918a08ef8f4192715c9faf401e8804
prateek-k0 <p.sudhakar.katkar@accenture.com>
2023-10-01 17:33:11 +0530

    markdown changes

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -39,7 +39,7 @@ const HomeBanner = ({ isLoadingData }) => {
     <Box sx={{ maxWidth: '100%', boxSizing: 'border-box', height: '100vh', backgroundColor: '#000', position: 'relative' }}>
       <animated.img style={{ width: '100vw', height: '100vh',  objectFit: 'cover', boxSizing: 'border-box', filter: to(filterStyles.x, val => `hue-rotate(${val}deg) ${isLoadingData ? 'none' : 'blur(10px)'}`)}} src={homepageBannerImage} alt="homepage-banner" />
       {!isLoadingData && 
-        <AnimatedBox style={{ position: 'absolute', height: '100%', width: '100%', top: '0', left: '0', paddingTop: '20%', boxSizing: 'border-box', ...headerStyles }}>
+        <AnimatedBox style={{ position: 'absolute', height: '100%', width: '100%', top: '0', left: '0', paddingTop: '15%', boxSizing: 'border-box', ...headerStyles }}>
           <Typography sx={{ color: '#fff', fontSize: '96px', fontWeight: '700', textAlign: 'center', fontFamily: 'monospace'}}>Crypto-Voyance</Typography>
           <Typography sx={{ color: '#fff', fontSize: '32px', fontWeight: '400', textAlign: 'center', margin: '20px 100px', fontFamily: 'monospace'}}>A cryptocurrency price tracker made with React & Redux Toolkit</Typography>
         </AnimatedBox>
